670e34c1a8cd8f6f0040853dbe44dd64f870e5f1
Dong Chen <dongluo.chen@docker.com>
2016-02-18 18:02:10 -0800

    Do not recycle connections. Let net/http handles connections.
    Signed-off-by: Dong Chen <dongluo.chen@docker.com>

--- api/handlers.go
+++ api/handlers.go
@@ -482,7 +482,6 @@ func getContainerJSON(c *context, w http.ResponseWriter, r *http.Request) {
 
 	// cleanup
 	defer resp.Body.Close()
-	defer closeIdleConnections(client)
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -791,7 +790,6 @@ func postContainersExec(c *context, w http.ResponseWriter, r *http.Request) {
 
 	// cleanup
 	defer resp.Body.Close()
-	defer closeIdleConnections(client)
 
 	// check status code
 	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
